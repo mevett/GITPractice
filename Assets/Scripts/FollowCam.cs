c22eafb91c4ed3848c6883d1681830aba894249a
mevett <mevett@emich.edu>
2015-11-01 20:34:52 -0500

    First Unity commit

---
+++ Assets/Scripts/FollowCam.cs
@@ -0,0 +1,66 @@
+﻿
+/**
+ * Author: Jeremy Gibson, with mods by Matt Evett
+ * 2014,5
+ */
+
+using UnityEngine;
+using System.Collections;
+
+public class FollowCam : MonoBehaviour {
+
+	static public FollowCam  S; // a FollowCam Singleton 
+	
+	// fields set in the Unity Inspector pane 
+	public float 			easing = 0.05f; // Eases the camera's movement/interpolation
+	public Vector2			minXY; // Default is [0,0].  Min value for camera's position
+	// Following variable name acts to "separate" the dynamically set variables
+	public bool              _____________________________; 
+	
+	// fields set dynamically 
+	public GameObject        poi; // The point of interest 
+	public float             camZ; // The desired Z pos of the camera 
+	
+	void Awake() { 
+		S = this; 
+		camZ = this.transform.position.z; 
+	} 
+	
+	void FixedUpdate () { 
+		Vector3 destination; 
+
+		// If there is no poi, return to P:[0,0,0], to center the camera
+		//  on the slingshot again
+		if (poi == null) { 
+			destination = Vector3.zero; //(N.B.: The slingshot is at the origin)
+		} else { 
+			// Get the position of the poi 
+			destination = poi.transform.position; 
+			// If poi is a Projectile, check to see if it's at rest 
+			if (poi.tag == "Projectile") { 
+				// if it is sleeping (that is, not moving) 
+				if ( poi.GetComponent<Rigidbody>().IsSleeping() ) { 
+					// return to default view 
+					poi = null; 
+					// in the next update 
+					return; 
+				} 
+			}
+		}
+
+		// Limit the X & Y against minimum values 
+		destination.x = Mathf.Max( minXY.x, destination.x ); 
+		destination.y = Mathf.Max( minXY.y, destination.y ); 
+		// Interpolate from current Camera position toward the destination
+		//   3rd arg determines the "weighting" between the 1st and 2nd args
+		destination = Vector3.Lerp (transform.position,destination, easing);
+		// Maintain destination.z (camera stays at same distance from XY-plane)
+		destination.z = camZ; 
+		// Set the camera to the destination 
+		transform.position = destination; 
+		// Set the orthographicSize of the Camera to keep Ground in view.
+		// Because the cameraY can never move below 0, by expanding the size of the
+		// view volume we can guarantee that the 
+		this.GetComponent<Camera>().orthographicSize = destination.y + 10; 
+	} 
+}
---
+++ Assets/Scripts/Slingshot.cs
@@ -0,0 +1,82 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Slingshot : MonoBehaviour {
+	// Fields set in Unity's Inspector pane
+	public GameObject prefabProjectile;
+	public float 			velocityMult = 4f;
+	//The name of the following variable will result in what looks like a "divider" line
+	//  in the Inspector pane.   Basically a nifty trick to add clarity to Unity's programmer interface.
+	public bool _______________________;
+	// Fields set dynamically
+	public GameObject        launchPoint; 
+	public Vector3           launchPos; 
+	public GameObject        projectile; 
+	public bool              aimingMode; 
+	static public Slingshot	 S;
+
+	
+	void Awake() { 
+		//Set the Slingshot singleton so that it can be accessed in ProjectileLine
+		S = this;
+
+		Transform launchPointTrans = transform.Find("LaunchPoint"); 
+		launchPoint = launchPointTrans.gameObject; 
+		launchPoint.SetActive( false ); 
+		launchPos = launchPointTrans.position;
+	} 
+
+	void OnMouseEnter() { 
+		//print("Slingshot:OnMouseEnter()"); 
+		launchPoint.SetActive(true);
+	} 
+	
+	void OnMouseExit() { 
+		//print("Slingshot:OnMouseExit()"); 
+		launchPoint.SetActive(false);
+	} 
+
+	void OnMouseDown() { 
+		// The player has pressed the mouse button while over Slingshot 
+		aimingMode = true; 
+		// Instantiate a Projectile 
+		projectile = Instantiate( prefabProjectile ) as GameObject; 
+		// Start it at the launchPoint 
+		projectile.transform.position = launchPos; 
+		// Set it to isKinematic for now.  This means that the object will not be moved by
+		//   the physics simulation, but will remain otherwise involved in the simulation
+		projectile.GetComponent<Rigidbody>().isKinematic = true; 
+	} 
+
+	void Update() { 
+		// If Slingshot is not in aimingMode, don't run this code 
+		if (!aimingMode) return; 
+
+		// Get the current mouse position in 2D screen coordinates 
+		Vector3 mousePos2D = Input.mousePosition; 
+		// Convert the mouse position to 3D world coordinates 
+		mousePos2D.z = -Camera.main.transform.position.z; 
+		Vector3 mousePos3D = Camera.main.ScreenToWorldPoint( mousePos2D ); 
+		// Find the delta from the launchPos to the mousePos3D 
+		Vector3 mouseDelta = mousePos3D-launchPos; 
+		// Limit mouseDelta to the radius of the Slingshot SphereCollider 
+		float maxMagnitude = this.GetComponent<SphereCollider>().radius; 
+		if (mouseDelta.magnitude > maxMagnitude) { 
+			mouseDelta.Normalize(); //Convert to unit vector 
+			mouseDelta *= maxMagnitude; 
+		} 
+		// Move the projectile to this new position 
+		Vector3 projPos = launchPos + mouseDelta; 
+		projectile.transform.position = projPos; 
+		
+		if ( Input.GetMouseButtonUp(0) ) { 
+			// The mouse has been released 
+			aimingMode = false; 
+			Rigidbody shotsBody = projectile.GetComponent<Rigidbody>();
+			shotsBody.isKinematic = false;  // We want physics simulation to handle it now
+			shotsBody.velocity = -mouseDelta * velocityMult; 
+			FollowCam.S.poi = projectile; // Set camera to follow the projectile
+			projectile = null; 
+		} 
+	} 
+}
